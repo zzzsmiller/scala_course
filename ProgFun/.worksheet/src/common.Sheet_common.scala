b82072c0159d1ca35e707459df4c05d8cc2ec23f
zzzsmiller <noname@email.no>
2013-04-30 13:31:34 +0400

    clear repo

--- ProgFun/.worksheet/src/common.Sheet_common.scala
+++
@@ -1,5 +0,0 @@
-package common
-
-object Sheet_common {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(81); 
-  println("Welcome to the Scala worksheet")}
-}
--- ProgFun/.worksheet/src/week04.Sheet_Nat.scala
+++
@@ -1,5 +0,0 @@
-package week04
-
-object Sheet_Nat {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(78); 
-  println("Welcome to the Scala worksheet")}
-}
